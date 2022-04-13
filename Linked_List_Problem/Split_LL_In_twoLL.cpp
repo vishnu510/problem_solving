2679d9161e32f260f788bf58a816b8013041137e
vishnu510 <vpal6044@gmail.com>
2022-04-14 00:39:55 +0530

    Linked List Problem

--- Linked_List_Problem/Split_LL_In_twoLL.cpp
+++ Linked_List_Problem/Split_LL_In_twoLL.cpp
@@ -1,135 +1,92 @@
-/* C++ Program to alternatively split
-a linked list into two halves */
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Link list node */
-class Node
+// A Linked List Node
+struct Node
 {
-	public:
 	int data;
-	Node* next;
+	struct Node* next;
 };
 
-/* pull off the front node of
-the source and put it in dest */
-void MoveNode(Node** destRef, Node** sourceRef) ;
 
-/* Given the source list, split its
-nodes into two shorter lists. If we number
-the elements 0, 1, 2, ... then all the even
-elements should go in the first list, and
-all the odd elements in the second. The
-elements in the new lists may be in any order. */
-void AlternatingSplit(Node* source, Node** aRef,
-							Node** bRef)
+void printList(struct Node* head)
 {
-	/* split the nodes of source
-	to these 'a' and 'b' lists */
-	Node* a = NULL;
-	Node* b = NULL;
-		
-	Node* current = source;
+	struct Node* ptr = head;
+	while (ptr)
+	{
+		printf("%d —> ", ptr->data);
+		ptr = ptr->next;
+	}
+
+	printf("NULL\n");
+}
+
+
+void push(struct Node** head, int data)
+{
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = data;
+	newNode->next = *head;
+	*head = newNode;
+}
+
+
+void moveNode(struct Node** destRef, struct Node** sourceRef)
+{
+
+	if (*sourceRef == NULL) {
+		return;
+	}
+
+	struct Node* newNode = *sourceRef;  
+	*sourceRef = (*sourceRef)->next;	
+	newNode->next = *destRef;   		
+	*destRef = newNode; 				
+}
+
+
+void alternatingSplit(struct Node* source, struct Node** aRef, struct Node** bRef)
+{
+	
+	struct Node* a = NULL;
+	struct Node* b = NULL;
+
+	struct Node* current = source;
+
 	while (current != NULL)
 	{
-		MoveNode(&a, ¤t); /* Move a node to list 'a' */
-		if (current != NULL)
-		{
-			MoveNode(&b, ¤t); /* Move a node to list 'b' */
+		moveNode(&a, &current); 		
+
+		if (current != NULL) {
+			moveNode(&b, &current); 	
 		}
 	}
+
 	*aRef = a;
 	*bRef = b;
 }
 
-/* Take the node from the front of
-the source, and move it to the front
-of the dest. It is an error to call
-this with the source list empty.
-	
-Before calling MoveNode():
-source == {1, 2, 3}
-dest == {1, 2, 3}
-		
-After calling MoveNode():
-source == {2, 3}	
-dest == {1, 1, 2, 3}	
-*/
-void MoveNode(Node** destRef, Node** sourceRef)
+int main(void)
 {
-	/* the front source node */
-	Node* newNode = *sourceRef;
-	assert(newNode != NULL);
-		
-	/* Advance the source pointer */
-	*sourceRef = newNode->next;
-		
-	/* Link the old dest off the new node */
-	newNode->next = *destRef;
-		
-	/* Move dest to point to the new node */
-	*destRef = newNode;
-}
+	
+	int keys[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int n = sizeof(keys)/sizeof(keys[0]);
 
-/* UTILITY FUNCTIONS */
-/* Function to insert a node at
-the beginning of the linked list */
-void push(Node** head_ref, int new_data)
-{
-	/* allocate node */
-	Node* new_node = new Node();
 	
-	/* put in the data */
-	new_node->data = new_data;
-	
-	/* link the old list off the new node */
-	new_node->next = (*head_ref);	
-	
-	/* move the head to point to the new node */
-	(*head_ref) = new_node;
-}
-
-/* Function to print nodes
-in a given linked list */
-void printList(Node *node)
-{
-	while(node!=NULL)
-	{
-	cout<<node->data<<" ";
-	node = node->next;
+	struct Node* head = NULL;
+	for (int i = n-1; i >= 0; i--) {
+		push(&head, keys[i]);
 	}
-}
 
-/* Driver code*/
-int main()
-{
-	/* Start with the empty list */
-	Node* head = NULL;
-	Node* a = NULL;
-	Node* b = NULL;
-	
-	/* Let us create a sorted linked list to test the functions
-	Created linked list will be 0->1->2->3->4->5 */
-	push(&head, 5);
-	push(&head, 4);
-	push(&head, 3);
-	push(&head, 2);
-	push(&head, 1);								
-	push(&head, 0);
-	
-	cout<<"Original linked List: ";
-	printList(head);
-	
-	/* Remove duplicates from linked list */
-	AlternatingSplit(head, &a, &b);
-	
-	cout<<"\nResultant Linked List 'a' : ";
-	printList(a);		
-	
-	cout<<"\nResultant Linked List 'b' : ";
-	printList(b);		
-	
+	struct Node *a = NULL, *b = NULL;
+	alternatingSplit(head, &a, &b);
+
+
+	printf("First List: ");
+	printList(a);
+
+	printf("Second List: ");
+	printList(b);
+
 	return 0;
 }
-
-// This code is contributed by rathbhupendra
