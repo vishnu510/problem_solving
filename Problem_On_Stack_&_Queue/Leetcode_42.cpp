5882d79ef829d3fc6d488aa6a9639cb0d4b5934b
vishnu510 <vpal6044@gmail.com>
2022-04-29 03:23:04 +0530

    Problem on stack & queue

--- Problem_On_Stack_&_Queue/Leetcode_42.cpp
+++ Problem_On_Stack_&_Queue/Leetcode_42.cpp
@@ -1,7 +1,28 @@
 // strapping rain water
 #include<bits/stdc++.h>
 using namespace std;
-
+// using stack
+int trap(vector<int > height){
+  int len =height.size();
+  stack<int> st;
+  int res =0;
+  int top,dist,boundHeight;
+for(int i=0;i<len;i++){
+    while (!st.empty() and height[i]>height[st.top()])
+    {
+       top = st.top();
+       st.pop();
+       if(st.empty()){
+           break;
+       }
+        dist = i- st.top()-1;
+        boundHeight = min(height[i],height[st.top()])-height[top];
+        res+=(dist*boundHeight);
+    }
+    st.push(i);
+}
+return res;
+}
 int main(){
     vector<int > height{0,1,0,2,1,0,1,3,2,1,2,1};
     int size = height.size();
@@ -22,6 +43,8 @@ int main(){
     {
         answer+= min(left[i],right[i])-height[i];
     }
-    cout<<answer;
+    cout<<answer<<endl;;
+    int ans = trap(height);
+    cout<<ans;
     return 0;
 }
