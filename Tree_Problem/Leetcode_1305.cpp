97cb8ce0a1614e188805bded1920544a269e24e8
vishnu510 <vpal6044@gmail.com>
2022-05-22 01:08:43 +0530

    Tree Problem

--- Tree_Problem/Leetcode_1305.cpp
+++ Tree_Problem/Leetcode_1305.cpp
@@ -20,19 +20,56 @@ TreeNode* builtTree(int val){
     return temp;
 }
 
-vector<int> results;
-void elements(TreeNode* root){
-        if(!root) return;
-        results.push_back(root->val);
-        elements(root->left);
-        elements(root->right);
+
+
+ void inOrderTraverse(TreeNode *root, vector<int>& vec)
+    {
+        if(root)
+	    {
+		    inOrderTraverse(root->left, vec);
+		    vec.push_back(root->val);
+		    inOrderTraverse(root->right, vec);
+
+	    }
     }
-vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        if(!(root1 || root2)) return {};
-        elements(root1);
-        elements(root2);
-        sort(results.begin(),results.end());
-        return results;
+	
+    void mergeVectors(vector<int> vec1, vector<int> vec2, vector<int>& mergedVectors)
+    {
+        int i = 0, j = 0, k = 0; 
+
+      
+        while (i < vec1.size() && j < vec2.size()) 
+        { 
+        
+            if (vec1[i] < vec2[j]) 
+                mergedVectors[k++] = vec1[i++]; 
+            else
+                mergedVectors[k++] = vec2[j++]; 
+        } 
+  
+    
+        while (i < vec1.size()) 
+            mergedVectors[k++] = vec1[i++]; 
+  
+    
+        while (j < vec2.size()) 
+            mergedVectors[k++] = vec2[j++]; 
+        
+        
+    } 
+    
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2)
+    {
+        vector<int> tree1, tree2;
+		
+        inOrderTraverse(root1, tree1);
+        inOrderTraverse(root2, tree2);
+        
+        vector<int> mergedVectors(tree1.size() + tree2.size());
+		
+        mergeVectors(tree1, tree2, mergedVectors);
+        return  mergedVectors;
+        
     }
 
 int main(){
