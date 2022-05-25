4c80108aff5272c05c9a34572e100cde372c3abb
vishnu510 <vpal6044@gmail.com>
2022-05-26 01:30:11 +0530

    String Problem

---
+++ Problem_on_String/Leetcode_451.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string frequencySort(string s){
+    priority_queue<pair<int,char>> MaxHeap;
+        unordered_map<char,int> map;
+        for(int i=0;i<s.size();i++)
+            map[s[i]]++;
+        for(auto i:map)
+            MaxHeap.push(make_pair(i.second,i.first));
+        string result="";
+        while(!MaxHeap.empty())
+        {
+            int freq=MaxHeap.top().first;
+            char str= MaxHeap.top().second;
+            for(int i=0;i<freq;i++)
+            {
+                result+=str;
+            }
+            MaxHeap.pop();
+        }
+        return result;
+}
+
+int main(){
+    string str = "tree";
+    cout<<frequencySort(str);
+    return 0;
+}
