65f9a5f054fb3478ee9e622705bf556ede6fa9a4
vishnu510 <vpal6044@gmail.com>
2022-06-01 03:37:50 +0530

    Problem on Dynamic Programming

---
+++ Problem_On_Dynamic_Programing/Leetcode_322.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+       vector<int> dp(amount+1,INT_MAX);
+       dp[0] = 0;
+       for(int i=1;i<=amount;i++){
+           for(int j=0;j<coins.size();j++){
+               if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX){
+                   dp[i] = min(dp[i],dp[i-coins[j]]+1);
+               }
+           }
+       }
+       return dp[amount]==INT_MAX?-1:dp[amount];
+    }
+
+int main(){
+     
+     vector<int> coins = {1,2,5};
+     int amount =11;
+     cout<<coinChange(coins,amount);
+     return 0;
+}
