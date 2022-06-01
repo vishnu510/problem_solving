11e01d40790b33de03486992e3adb92fae5acf65
vishnu510 <vpal6044@gmail.com>
2022-06-02 00:17:32 +0530

    Problem on Dynamic Programming

--- Problem_On_Dynamic_Programing/Leetcode_72.cpp
+++ Problem_On_Dynamic_Programing/Leetcode_72.cpp
@@ -3,25 +3,30 @@
 using namespace std;
 
 int minDistance(string word1, string word2) {
-        int x = word1.size(),y = word2.size();
-        int dp[x+1][y+1];
-        for(int i=0;i<x;i++){
-            dp[0][i] =i;
-        }
-         for(int i=0;i<y;i++){
-            dp[i][0] =i;
-        }
-        for(int i=1;i<x;i++){
-            for(int j=1;j<y;j++){
-                if(word1[j-1]==word2[i-1]){
-                    dp[i][j] = dp[i-1][j-1];
+        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,-1));
+        for(int i=0;i<word1.length()+1;i++){
+            for(int j=0;j<word2.length()+1;j++){
+                if(i==0){
+                    dp[i][j] = j;
+                }
+                else if(j==0){
+                    dp[i][j] =i;
                 }
                 else{
-                    dp[i][j] =min(dp[i-1][j]+1,min(dp[i][j-1]+1,dp[i-1][j-1]));
+                    if(word1[i-1]==word2[j-1]){
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                    else{
+                        int add = dp[i][j-1];
+                        int replace = dp[i-1][j-1];
+                        int remove = dp[i-1][j];
+                        dp[i][j] = 1+min(min(replace,remove),add);
+                    }
+                    
                 }
             }
         }
-        return dp[x-1][y-1];
+        return dp[word1.length()][word2.length()];
     }
 
 int main(){
