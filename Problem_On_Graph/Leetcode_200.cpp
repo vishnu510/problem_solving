0ce3f41c8cc7fe703709a93e98317af0e7a077fb
vishnu510 <vpal6044@gmail.com>
2022-06-04 07:34:08 +0530

    Problem on Graph

---
+++ Problem_On_Graph/Leetcode_200.cpp
@@ -0,0 +1,41 @@
+//200. Number of Islands
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<vector<char>>& grid,int r,int c,int nr,int nc){
+    grid[r][c] = '0';
+    if(r-1>=0 && grid[r-1][c]=='1'){
+        dfs(grid,r-1,c,nr,nc);
+    }
+    if(r+1<nr && grid[r+1][c]=='1'){
+        dfs(grid,r+1,c,nr,nc);
+    }
+    if(c-1>=0 && grid[r][c-1]=='1'){
+        dfs(grid,r,c-1,nr,nc);
+    }
+    if(c+1<nc && grid[r][c+1]=='1'){
+        dfs(grid,r,c+1,nr,nc);
+    }
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    int row = grid.size();
+    int col = grid[0].size();
+    int numOfIsland = 0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(grid[i][j]=='1'){
+                dfs(grid,i,j,row,col);
+                numOfIsland++;
+            }
+        }
+    }
+    return numOfIsland;
+}
+
+int main(){
+
+    vector<vector<char>> grid = {{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
+    cout<<numIslands(grid);
+    return 0;
+}
